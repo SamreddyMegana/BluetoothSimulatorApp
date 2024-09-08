5564e1466e6a54339fb1f55582f67f4ff73f8888
Samreddy Megana <samreddy.megana@iorta.in>
2024-09-08 13:34:56 +0530

    fixes

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|react-native-ble-plx)/)',
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
 };
